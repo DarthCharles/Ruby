9d32356de15be1fe7aa90da4a9ae100132d84562
Charles <carlos.darth@gmail.com>
2014-04-28 00:39:51 -0700

    Se agregó  lo de las palabras, oraciones y parrafos

--- TextAnalizer.rb
+++ TextAnalizer.rb
@@ -11,3 +11,12 @@ puts "#{total_characters} characters"
 total_characters_nospaces = text.gsub(/\s+/, '').length
 
 puts "#{total_characters_nospaces} characters excluding spaces"
+
+word_count = text.split(' ').length
+puts "#{word_count} words"
+
+sentence_count = text.split(/\.|\?|!/).length
+puts "#{sentence_count} sentences"
+
+paragraph_count = text.split(/\n\n/).length
+puts "#{paragraph_count} paragraphs"
